dfc80d65606df583b57bcfb4901f50e263257f49
dimw <dimw@gmx.de>
2021-04-26 15:42:54 +0200

    Add node_modules; remove Jest

--- src/deployments.service.test.js
+++
@@ -1,8 +0,0 @@
-const { createDeployment } = require('./deployments.service');
-
-
-it('should return failure', async () => {
-  const data = await createDeployment('sendinblue/deployment-action', {}, 'xxx');
-
-  console.log(data);
-})
